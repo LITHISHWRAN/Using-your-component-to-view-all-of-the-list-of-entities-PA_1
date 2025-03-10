84e22018b85e8642264fa5e32fced654bcbcfc01
LITHISHWARAN <lithishwaran.v.s75@kalvium.community>
2025-03-10 11:42:43 +0530

    Assignment competed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-
+import BookCard from './components/bookcard';
+import "./App.css"
 function App() {
   const books = [
     {
@@ -34,7 +35,11 @@ function App() {
     },
   ];
 
-  return <>{/* write code to display all the books using */}</>;
+  return <>
+    {books.map((book, index) => (
+      <BookCard key={index} book={book} />
+    ))}
+  </>;
 }
 
 export default App;
